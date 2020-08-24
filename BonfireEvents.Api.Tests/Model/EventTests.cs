530953d29140be6be03b42db15729cf8bda893ac
David Laribee <laribee@gmail.com>
2020-08-24 14:04:04 -0400

    GH-1 - Event has a description.
    ### Guidance
    
    Notice how making this test created failures in other tests?

--- BonfireEvents.Api.Tests/Model/EventTests.cs
+++ BonfireEvents.Api.Tests/Model/EventTests.cs
@@ -9,7 +9,7 @@ namespace BonfireEvents.Api.Tests.Model
     [Fact]
     public void Event_has_a_title()
     {
-      Event subject = new Event(title: @"My C# Event");
+      Event subject = new Event(title: @"My C# Event", description: "Monthly meet-up for enthusiasts.");
       
       Assert.Equal("My C# Event", subject.Title);
     }
@@ -17,19 +17,29 @@ namespace BonfireEvents.Api.Tests.Model
     [Fact]
     public void Title_is_required()
     {
-      Assert.Throws<CreateEventException>(() => new Event(title: null));
+      Assert.Throws<CreateEventException>(() => new Event(title: null, description: "Monthly meet-up for enthusiasts."));
+    }
+
+    [Fact]
+    public void Event_has_a_description()
+    {
+      Event subject = new Event(title: @"My C# Event", description: "Monthly meet-up for enthusiasts.");
+      
+      Assert.Equal("Monthly meet-up for enthusiasts.", subject.Description);
     }
   }
 
   public class Event
   {
-    public Event(string title)
+    public Event(string title, string description)
     {
       if (string.IsNullOrEmpty(title)) throw new CreateEventException();
       Title = title;
+      Description = description;
     }
 
     public string Title { get; }
+    public string Description { get; }
   }
 
   public class CreateEventException : Exception
