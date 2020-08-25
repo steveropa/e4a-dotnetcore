9c6173fc0e95fdd5637184e66d56e40881620e2a
David Laribee <laribee@gmail.com>
2020-08-25 13:51:59 -0400

    GH-1 - Register a `FakeEventRepository` with DI
    Notes:
    
    - Register FakeEventRepository with DI

---
+++ BonfireEvents.Api/Storage/FakeEventRepository.cs
@@ -0,0 +1,18 @@
+using System.Threading.Tasks;
+using BonfireEvents.Api.Model;
+
+namespace BonfireEvents.Api.Storage
+{
+  /// <summary>
+  /// Since we haven't decided on a persistence technology yet, we'll
+  /// rely on a fake for exploratory testing purposes (testing through
+  /// browser, PostMan, etc. w/ API running).
+  /// </summary>
+  public class FakeEventRepository : IEventRepository
+  {
+    public Event Find(int id)
+    {
+      return new Event("Fake", "Fake");
+    }
+  }
+}
